0d836c570211c38039a6aae9856d4c3de1845e57
Ramil Saavedra <ramilsmcl@gmail.com>
2022-05-17 16:24:45 +0800

    add fecth button / render todo list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,8 +8,23 @@ interface AppProps {
   fetchTodos(): any;
 }
 class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
