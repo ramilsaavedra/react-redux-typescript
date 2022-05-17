e52fc5ab3b78ff62e2c9cd6911e539e0a6f53a2b
Ramil Saavedra <ramilsmcl@gmail.com>
2022-05-17 13:30:10 +0800

    add interface for dispatch(fetchTodos)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configureStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { App } from './components/App';
 import { reducers } from './reducers';
 
-const store = configureStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
